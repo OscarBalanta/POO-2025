956c9284725d994e677765f5f9b17d261748e872
OscarBalanta <oscar.balantav@campusucc.edu.co>
2025-08-27 22:02:32 +0000

    Practica_condicionales.java

---
+++ Clase #4/Practica_condicionales.java
@@ -0,0 +1,71 @@
+public class Practica_condicionales {
+    public static void main(String[] args) {
+
+    /*
+     * Ejercicio 4.1.a)
+     * Si la temperatura es superior a 100 grados, visualizar el
+     * mensaje “por encima del punto de ebullición del agua” sino  
+     * Visualizar el mensaje “por debajo del punto de ebullición del agua”
+     * Entrada Temperatua
+     * Proceso:Validar que la temperatura sea superior a 100 grados
+     * Salida: si alcaanza o no el punto de ebulición 
+    */
+
+     int angulo = 90;
+     if ( angulo ==90){ 
+        System.out.println("El angulo es un angulo recto");
+        } else 
+            {
+                System.out.println("El angulo no es un angulo recto");
+                
+            }
+        
+
+         /*
+        * Ejercicio 4.1.a)
+        * Si la temperatura es superior a 100 grados, visualizar el
+        * mensaje “por encima del punto de ebullición del agua” sino  
+        * Visualizar el mensaje “por debajo del punto de ebullición del agua”
+        * Entrada Temperatua
+        * Proceso:Validar que la temperatura sea superior a 100 grados
+        * Salida: si alcaanza o no el punto de ebulición 
+        */
+
+            double Temperatura = 30.0;
+            if (Temperatura> 100){
+                System.out.println("por encima de  el punto de ebullición del agua");
+            } else 
+                {
+                    System.out.println("pordebajo de  el punto de ebullición del agua");
+                }
+                   
+
+        /*
+        * Ejercicio 4.1.a)
+        * Si  el número es positivo, sumar el , sumar el número a total
+        * de positivos, sino 
+        * sumar al total de negativos
+        * Entrada: Numero
+        * Proceso: Validar si el Numero es positivo o negtivo
+        * Salida: Validar el total de numeros positivos y negativos 
+        */
+
+        double Numeros = 0;
+        double Totalpositivos =0;
+        double Totalnegativos =0;
+        if ( Numeros >=0 ){
+            Totalpositivos = Totalpositivos =  Numeros;
+            System.out.println("El numero es Positivo");
+            } else {
+                System.out.println("El numero es negativo");
+            }
+        }
+}
+
+
+
+
+
+   
+
+    
