0fb7b76ea789c3a72f0d2400d6dde39d7dc48c74
OscarBalanta <oscar.balantav@campusucc.edu.co>
2025-09-08 23:14:02 +0000

    Practica_condicionales.javaACT

--- Practica_condicionales.java
+++ Practica_condicionales.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Practica_condicionales {
     public static void main(String[] args) {
 
@@ -50,16 +52,76 @@ public class Practica_condicionales {
         * Salida: Validar el total de numeros positivos y negativos 
         */
 
-        double Numeros = 0;
-        double Totalpositivos =0;
-        double Totalnegativos =0;
-        if ( Numeros >=0 ){
-            Totalpositivos = Totalpositivos =  Numeros;
-            System.out.println("El numero es Positivo");
-            } else {
-                System.out.println("El numero es negativo");
-            }
+        int numero = -5;
+        
+        if(numero > 0){
+            System.out.println("El número es positivo");
+        }else if(numero < 0){
+            System.out.println("El número es negativo");
+        }else{
+            System.out.println("El número es cero");
         }
+
+        int Num = 0;
+        double x = 3;
+        double y = 1;
+        double z = 12;
+        double p = 0;
+        if(x > y && z < 20){
+          System.out.println(p);
+        }
+
+
+
+        double distancia = 23;
+        double tiempo = 0;
+        if(distancia > 20 && distancia < 35){
+            System.out.println(tiempo);
+        }
+
+        int NUM1 = 10;
+        int NUM2 = 122;
+        if(NUM1 > NUM2){
+         System.out.println("El primer numero es el mayor");
+        }else if(NUM1 < NUM2){
+            System.out.println("El primer numero es el mas pequeño");
+        } else {
+            System.out.println("Ambos numeros tienen el mismo valor");
+        }
+
+        int Nmenor = 100;
+        int Ncentral = 150;
+        int Nmayor = 200;
+        double NumCentral = 0;
+        if (Nmenor < Ncentral && Nmayor > Ncentral){
+              System.out.println("El numero central es " +Ncentral);
+        }
+
+       
+       double NRAIZ = 25;
+        System.out.println(Math.sqrt(NRAIZ));
+       
+        
+          Scanner teclado = new Scanner(System.in);
+       
+        int NUMBER;
+       
+        System.out.print("ingrese el Numero: ");
+        NUMBER = teclado.nextInt();
+       
+        if(NUMBER % 2 == 0) {
+            System.out.println(NUMBER+" es un número par");
+        }else {
+            System.out.println(NUMBER+" no es un número par");
+        }
+
+       
+       
+
+    } 
+
+        
+       
 }
 
 
