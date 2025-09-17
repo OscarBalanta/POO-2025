e8c84117967ec87a115b19ea2471df78b4dfde3a
OscarBalanta <oscar.balantav@campusucc.edu.co>
2025-09-17 22:15:55 +0000

    Ejecutarpersona.java

---
+++ clase #7/Ejecutarpersona.java
@@ -0,0 +1,11 @@
+public class Ejecutarpersona {
+    public static void main(String[] args) {
+       
+        //Creacion del objeto
+        //El operador "new" me permite crear el objeto
+
+        Persona objpersona1 = new Persona(1107851406, )
+        
+    }
+    
+}
